6e02f1e5cb96beeee032148a5c67b96850a83974
Vincent Loh <vincent.ml@gmail.com>
2020-05-11 15:39:59 +0800

    make sheet protocol explicit https://

--- hooks/useBaseUrl.ts
+++ hooks/useBaseUrl.ts
@@ -1,11 +1,11 @@
-import { useRouter } from "next/router";
+import { useRouter } from 'next/router'
 
 const useBaseUrl = (id?: any) =>
-  ["1qg2g3E1F1o6cIpt5E6gDh0Ctv-8btBTEY2AYoXJ73eM"]
+  ['1qg2g3E1F1o6cIpt5E6gDh0Ctv-8btBTEY2AYoXJ73eM']
     .map((fallbackId) => id || useRouter().query.id || fallbackId)
     .map(
       (id) =>
-        `//docs.google.com/spreadsheets/d/${id}/gviz/tq?tqx=out:csv&sheet=`
-    )[0];
+        `https://docs.google.com/spreadsheets/d/${id}/gviz/tq?tqx=out:csv&sheet=`
+    )[0]
 
-export default useBaseUrl;
+export default useBaseUrl
