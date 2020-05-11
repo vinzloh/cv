0cec38a0aab483628da0ab33faa3021b64db73c3
Vincent Loh <vincent.ml@gmail.com>
2020-05-11 20:39:05 +0800

    add eslint and typescript-eslint, enforce in build

---
+++ next.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  webpack: (config, { dev }) => {
+    if (dev) {
+      config.module.rules.push({
+        test: /\.(js|jsx|ts|tsx)$/,
+        loader: 'eslint-loader',
+        exclude: ['/node_modules/'],
+        enforce: 'pre',
+        options: {
+          emitWarning: true,
+          fix: true,
+        },
+      })
+    }
+    return config
+  },
+}
