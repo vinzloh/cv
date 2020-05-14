f2aedd1e0a5cdb7a2ba84625e3c33d59d71b022a
Vincent Loh <vincent.ml@gmail.com>
2020-05-14 16:49:54 +0800

    separate transforms for each url

--- pages/troll/[id].tsx
+++ pages/troll/[id].tsx
@@ -1,58 +1,72 @@
 import SheetRenderer from 'components/SheetRenderer'
 import { Hash } from 'definitions'
-import { getArrayValue } from 'helpers'
+import { getArrayValue, hasKeys } from 'helpers'
 import useSheet from 'hooks/useSheet'
 import fetch from 'isomorphic-unfetch'
 import { useEffect, useState } from 'react'
 
+interface Task {
+  url: string
+  transforms: string
+}
+
 export default function Troll() {
   const config = useSheet('config').data as []
-  const [tasksUrl, setTasksSheet] = useState('')
-  const tasks = useSheet(tasksUrl).data as []
+  const [tasks, setTasks] = useState<Task[]>([])
+  const [transformsHash, setTransformsHash] = useState<Hash>({})
+
+  const [tasksUrl, setTasksUrl] = useState('')
+  useEffect(() => {
+    if (config) setTasksUrl(getArrayValue(config, 'tasks'))
+  }, [config])
+
+  const tasksResponseData = useSheet(tasksUrl).data as []
+  useEffect(() => {
+    if (tasksResponseData) setTasksToPrep(tasksResponseData)
+  }, [tasksResponseData])
+
   const [transformUrl, setTransformUrl] = useState('')
+  const [tasksToPrep, setTasksToPrep] = useState<Task[]>([])
+  useEffect(() => {
+    if (tasksToPrep.length === 0) {
+      if (hasKeys(transformsHash)) setTasks(tasksResponseData)
+      return
+    }
+    setTransformUrl(tasksToPrep[0].transforms)
+  }, [tasksToPrep, transformsHash, tasksResponseData])
+
   const transforms = useSheet(transformUrl).data as []
+  useEffect(() => {
+    if (!transforms || transformUrl.length === 0) return
+    setTransformsHash((prev) => ({ ...prev, [transformUrl]: transforms }))
+    setTransformUrl('')
+    setTasksToPrep((prev) =>
+      prev.filter((task) => task.transforms !== transformUrl)
+    )
+  }, [transforms, transformUrl, setTransformUrl, transformsHash])
+
+  const [results, setResults] = useState<any[]>([])
 
   useEffect(() => {
-    if (!config) return
-    console.table(config)
-  }, [config])
+    if (tasks.length === 0 || results.length > 0) return
+    if (tasks.some((task) => !transformsHash[task.transforms])) return
 
-  useEffect(() => {
-    if (!config) return
-    setTasksSheet(getArrayValue(config, 'tasks'))
-  }, [config])
-
-  useEffect(() => {
-    if (!tasks) return
-    // TODO: transforms of each row
-    const [firstTask] = tasks as any
-    setTransformUrl((firstTask || ({} as any)).transforms)
-  }, [tasks])
-
-  const [results, setResults] = useState([])
-
-  useEffect(() => {
-    if (!tasks && !transforms) return
-
-    console.group(`tasks ${tasksUrl}`)
+    console.group(`tasks`)
     console.table(tasks)
     console.groupEnd()
 
-    tasks.forEach((task: Hash) => {
-      fetch(`/api/sauron`, {
-        method: 'POST',
-        body: JSON.stringify({
-          url: task.url,
-          trolls: transforms,
-        }),
-      })
-        .then((r) => r.json())
-        .then((res: any) => {
-          if (res.status === false) return
-          setResults((prev) => prev.concat(res))
-        })
-    })
-  }, [tasks, tasksUrl, config, transforms])
+    Promise.all(
+      tasks.map((task: Task) =>
+        fetch(`/api/sauron`, {
+          method: 'POST',
+          body: JSON.stringify({
+            url: task.url,
+            trolls: transformsHash[task.transforms as any],
+          }),
+        }).then((r) => r.json())
+      )
+    ).then(setResults)
+  }, [tasks, results, tasksUrl, config, transformsHash])
 
   return (
     <>
