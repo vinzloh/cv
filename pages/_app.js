4569f9061c29c79a88bdc0d9ef6ea3afd075ba50
Vincent Loh <vincent.ml@gmail.com>
2020-05-14 09:24:52 +0800

    refactor base layout into _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,9 @@
-import "styles/index.css";
+import 'styles/index.css'
 
-export default ({ Component, pageProps }) => <Component {...pageProps} />;
+export default ({ Component, pageProps }) => (
+  <div className="container h-full mx-auto flex">
+    <main className="flex flex-col m-auto justify-center items-center">
+      <Component {...pageProps} />
+    </main>
+  </div>
+)
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,5 @@
-import SheetRenderer from "components/SheetRenderer";
+import SheetRenderer from 'components/SheetRenderer'
 
 export default function Home() {
-  return (
-    <div className='container h-full mx-auto flex'>
-      <main className='flex flex-col m-auto justify-center items-center'>
-        <SheetRenderer />
-      </main>
-    </div>
-  );
+  return <SheetRenderer />
 }
