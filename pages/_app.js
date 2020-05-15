bde59f53c7db943c05633751ec907d43fa6e61c3
Vincent Loh <vincent.ml@gmail.com>
2020-05-15 15:14:22 +0800

    add web vitals reporting

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import 'styles/index.css'
 
+// Will be called once for every metric that has to be reported.
+export function reportWebVitals(metric) {
+  // These metrics can be sent to any analytics service
+  console.log(metric)
+}
+
 export default ({ Component, pageProps }) => (
   <div className="container h-full mx-auto flex">
     <main className="flex flex-col m-auto justify-center items-center">
