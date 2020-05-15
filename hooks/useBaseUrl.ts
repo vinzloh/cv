3d374c58da3c4fefba0e1cbce8a38d10213c1b41
Vincent Loh <vincent.ml@gmail.com>
2020-05-15 21:43:07 +0800

    refactor helper classes

--- hooks/useBaseUrl.ts
+++ hooks/useBaseUrl.ts
@@ -1,13 +1,12 @@
 import { useRouter } from 'next/router'
 
+export const getBaseUrl = (
+  id: string = '1qg2g3E1F1o6cIpt5E6gDh0Ctv-8btBTEY2AYoXJ73eM'
+) => `https://docs.google.com/spreadsheets/d/${id}/gviz/tq?tqx=out:csv&sheet=`
+
 const useBaseUrl = (id?: any) => {
   const router = useRouter()
-  return ['1qg2g3E1F1o6cIpt5E6gDh0Ctv-8btBTEY2AYoXJ73eM']
-    .map((fallbackId) => id || router.query.id || fallbackId)
-    .map(
-      (id) =>
-        `https://docs.google.com/spreadsheets/d/${id}/gviz/tq?tqx=out:csv&sheet=`
-    )[0]
+  return getBaseUrl(id || router.query.id)
 }
 
 export default useBaseUrl
--- hooks/useSheet.ts
+++ hooks/useSheet.ts
@@ -1,8 +1,18 @@
-import { Hash } from 'definitions'
+import { GoogleSheet, Hash } from 'definitions'
 import { papaConfig } from 'helpers'
-import useBaseUrl from 'hooks/useBaseUrl'
+import useBaseUrl, { getBaseUrl } from 'hooks/useBaseUrl'
 import Papa from 'papaparse'
 import { useEffect, useState } from 'react'
+
+export const getSheet = (
+  id: string,
+  name: string,
+  callback: (T: GoogleSheet) => void
+) =>
+  Papa.parse(getBaseUrl(id) + name, {
+    ...papaConfig,
+    complete: (results) => callback(results),
+  })
 
 export default function useSheet(name: string = ''): Hash {
   const [sheet, setSheet] = useState({})
