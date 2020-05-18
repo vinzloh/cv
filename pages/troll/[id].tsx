e51b9b64fbf4cbbc83e63c7167b3ca783031f156
Vincent Loh <vincent.ml@gmail.com>
2020-05-19 00:19:28 +0800

    switch from nested callbacks to promise chains

--- pages/troll/[id].tsx
+++ pages/troll/[id].tsx
@@ -3,6 +3,7 @@ import SheetRenderer from 'components/SheetRenderer'
 import { findValue } from 'helpers'
 import { getSheet } from 'hooks/useSheet'
 import { Fragment, useEffect, useState } from 'react'
+import { GoogleSheet } from 'definitions'
 
 interface Task {
   url: string
@@ -21,28 +22,46 @@ export default function Troll(props: any) {
 
   useEffect(() => {
     if (!id) return
-    getSheet(id, 'config', (res) => {
+
+    const setPageStyle = (res: GoogleSheet) => {
       setPageClassName(findValue(res.data as [], 'className'))
-      const taskSheetName = findValue(res.data as [], 'tasks')
-      getSheet(id, taskSheetName, (res) =>
-        (res.data as Task[]).forEach((task, taskIndex) =>
-          getSheet(id, task.transforms as string, (res) =>
-            fetch(`/api/sauron`, {
-              method: 'POST',
-              body: JSON.stringify({ url: task.url, trolls: res.data }),
-            })
-              .then((r) => r.json())
-              .then((sheet: TransformSheet) =>
-                setSheets((prev) => {
-                  const newSheets = [...prev]
-                  newSheets[taskIndex] = sheet
-                  return newSheets
-                })
-              )
-          )
+      return res
+    }
+
+    const getTasksSheetName = (res: GoogleSheet) =>
+      findValue(res.data as [], 'tasks')
+
+    const getTasks = (taskSheetName: string) => getSheet(id, taskSheetName)
+    const toTaskArray = (res: GoogleSheet) => res.data as Task[]
+
+    const getTaskTransforms = (task: Task) =>
+      getSheet(id, task.transforms as string)
+
+    const fetchData = (task: Task) => (res: GoogleSheet) =>
+      fetch(`/api/sauron`, {
+        method: 'POST',
+        body: JSON.stringify({ url: task.url, trolls: res.data }),
+      }).then((r) => r.json())
+
+    const updateSheets = (taskIndex: number) => (sheet: TransformSheet) =>
+      setSheets((prev) => {
+        const newSheets = [...prev]
+        newSheets[taskIndex] = sheet
+        return newSheets
+      })
+
+    getSheet(id, 'config')
+      .then(setPageStyle)
+      .then(getTasksSheetName)
+      .then(getTasks)
+      .then(toTaskArray)
+      .then((tasks: Task[]) =>
+        tasks.forEach((task, index) =>
+          getTaskTransforms(task)
+            .then(fetchData(task))
+            .then(updateSheets(index))
         )
       )
-    })
   }, [id])
 
   return (
