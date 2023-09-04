84995a5c1e28eaee017f093bd7bbfdd4df2b3027
Vince Loh <5501845+vinzloh@users.noreply.github.com>
2023-09-04 21:41:10 +0800

    swap `tailwind` for `twind`

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
 import vercelServerless from '@astrojs/vercel/serverless';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   adapter: vercelServerless({
-    functionPerRoute: false,
+    functionPerRoute: false, // only works when false
   }),
-  integrations: [react(), tailwind()],
+  integrations: [react()],
   output: 'server',
 });
