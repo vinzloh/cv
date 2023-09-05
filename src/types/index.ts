d4246420447803aa6773ef9a15397f34e2337890
Vincent <5501845+vinzloh@users.noreply.github.com>
2023-09-06 01:26:50 +0800

    Fix/setup lint n strong type (#83)
    * setup astro eslint, add `lint` script to `package.json`
    
    * fix errors from `pnpm lint`
    
    * add `pnpm lint` to github action
    
    * strong type `any`

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,8 +1,25 @@
-export interface Hash<T = {}> {
-  [key: string]: T
+export type FieldValues = Record<string, string | null>;
+
+export interface SheetStyles {
+  [key: string]: Array<{
+    key: string;
+    className?: string;
+  }>;
+}
+
+export interface SheetData {
+  [x: string]: GoogleSheet;
 }
 
 export interface GoogleSheet {
-  meta: { fields: string[] }
-  data: any[]
+  meta: {
+    aborted?: boolean;
+    cursor?: number;
+    delimiter?: string;
+    fields: string[];
+    linebreak?: string;
+    truncated?: boolean;
+  };
+  data: FieldValues[];
+  errors: unknown[];
 }
